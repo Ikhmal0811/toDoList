7e511c39a7122c0b5905b31f391f4b2ee85d79c5
malmi <mhd1ikhmal@gmail.com>
2025-03-17 21:12:43 +0800

    create 3 file for the project

---
+++ main.js
@@ -0,0 +1,11 @@
+const pushButton = document.querySelector("button")
+let getInput = document.querySelector('input')
+
+let saveInput = {}
+
+
+
+
+pushButton.addEventListener("click",function(){
+        
+})
