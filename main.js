c79f74eeb5f88da1b9140619b330042be1a9fc90
malmi <mhd1ikhmal@gmail.com>
2025-03-19 22:44:19 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-const pushButton = document.querySelector("button")
-let getInput = document.querySelector('input')
 
-let saveInput = {}
+let inputToDo = document.getElementById("inputToDo");
+let convertButton = document.getElementById("jsonbtn");
+let jsonText = document.getElementById("jsontext")
+
+// let storeList = []
 
 
 
+let count = 0;
 
-pushButton.addEventListener("click",function(){
-        
+
+convertButton.addEventListener("click", function(){
+     
+    //  storeList.push(inputToDo.value);
+
+     localStorage.setItem(`inputList${count}`,inputToDo.value);
+
+     console.log(localStorage.getItem(localStorage.key(0)));
+     console.log(localStorage.length);
+     count++
+    
 })
+
+
